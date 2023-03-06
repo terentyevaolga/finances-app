5f4b6aac8ee192ebadec49089079989e25a8c4cb
terentyevaolga <terentyeevaolga@gmail.com>
2023-03-06 17:37:43 +0300

    upd

--- webapp/models.py
+++ webapp/models.py
@@ -1,17 +1,3 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 
-
-User = get_user_model()
-
-
-class MoneySlotTag(models.Model):
-    title = models.CharField(max_length=256)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-
-class MoneySlot(models.Model):
-    title = models.CharField(max_length=256)
-    amount_spent = models.IntegerField
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(MoneySlotTag)
+# Create your models here.
