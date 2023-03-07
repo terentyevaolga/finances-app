454f3855f5695381b054da1d35e19097be16133a
terentyevaolga <terentyeevaolga@gmail.com>
2023-03-07 09:59:19 +0300

    add postgres

--- finances/settings.py
+++ finances/settings.py
@@ -75,10 +75,22 @@ WSGI_APPLICATION = 'finances.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_finances',
+        'USER': 'postgres',
+        'PASSWORD': 'aser4321',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
